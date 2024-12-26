3b035fb23c38bf7fb19a21c6b5f4b900d84f5116
Joseph Gu <josephgu2003@gmail.com>
2024-12-26 11:16:25 -0500

    integrate options in config files

--- ezflow/models/dino.py
+++ ezflow/models/dino.py
@@ -8,7 +8,7 @@ from ezflow.encoder.dinov2.native_attention import NativeAttention
 from ezflow.encoder.dinov2.vision_transformer import DinoVisionTransformer, vit_small
 from ezflow.models.build import MODEL_REGISTRY
 from ezflow.models.dit import get_2d_sincos_pos_embed
-from ezflow.modules.decoder import DecoderBlock
+from ezflow.modules.decoder import DecoderBlock, ConcatenateDecoderBlock
 from ezflow.modules.base_module import BaseModule 
 from ezflow.utils.invert_flow import reparameterize
 
@@ -25,11 +25,17 @@ class Dino(BaseModule):
         self.final_layer = nn.Linear(cfg.HIDDEN_SIZE, 3)
         num_patches = 32 * 32 * self.scaler * self.scaler
         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, cfg.HIDDEN_SIZE), requires_grad=False)
+        
+        block_classes = {'DecoderBlock': DecoderBlock, 'ConcatenateDecoderBlock': ConcatenateDecoderBlock}
+        block_class = block_classes[cfg.BLOCK_CLASS]
         self.decoder_blocks = nn.ModuleList([
-            DecoderBlock(cfg.HIDDEN_SIZE, cfg.NUM_HEADS) for _ in range(cfg.DECODER_BLOCKS)
+            block_class(cfg.HIDDEN_SIZE, cfg.NUM_HEADS) for _ in range(cfg.DECODER_BLOCKS)
         ])
         self.init_weights()
         self.vits16 = dinov2_vits14_reg(block_fn=partial(Block, attn_class=NativeAttention))
+        
+        self.encoder_concat = cfg.ENCODER_CONCAT 
+        self.reparam = cfg.REPARAM
       
     def init_weights(self):
         def _basic_init(module):
@@ -63,12 +69,13 @@ class Dino(BaseModule):
         both_img = simple_interpolate(both_img, size=(448 * self.scaler, 448 * self.scaler)) # TODO: fix
 
         x = self.vits16.prepare_tokens_with_masks(both_img, None)
+       
+        if self.encoder_concat: 
+            x1x2 = torch.chunk(x, 2)
+            img1 = x1x2[0] # TODO: img encoding??
+            img2 = x1x2[1]
         
-        x1x2 = torch.chunk(x, 2)
-        img1 = x1x2[0] # TODO: img encoding??
-        img2 = x1x2[1]
-        
-        x = torch.concat((img1, img2), dim=1)
+            x = torch.concat((img1, img2), dim=1)
         
         for blk in self.vits16.blocks:
             x = blk(x)
@@ -76,7 +83,7 @@ class Dino(BaseModule):
         
         x = self.proj(x)
         
-        x1x2 = torch.chunk(x, 2, dim=1)
+        x1x2 = torch.chunk(x, 2, dim=1 if self.encoder_concat else 0)
         x1 = x1x2[0][:, 1+4:] + self.pos_embed
         x2 = x1x2[1][:, 1+4:] + self.pos_embed
         
@@ -88,7 +95,11 @@ class Dino(BaseModule):
         q = self.final_layer(q) 
         q = self.unpatchify(q)
 
-        flow = reparameterize(q[:, :2, :, :], hw)
+        if self.reparam:
+            flow = reparameterize(q[:, :2, :, :], hw)
+        else:
+            flow = torch.nn.functional.interpolate(q[:, :2, :, :], size=hw, mode='bilinear', align_corners=True)
+            
         #var = simple_interpolate(flow[:, -1:, :, :], size=hw)
         var = torch.nn.functional.interpolate(q[:, -1:, :, :], size=hw, mode='bilinear', align_corners=True)
 
