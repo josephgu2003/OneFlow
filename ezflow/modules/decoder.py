6d6b7a639e0e52235ebcd3bd364bcc489577b07a
Joseph Gu <josephgu2003@gmail.com>
2024-12-31 10:10:55 -0500

    try fan

--- ezflow/modules/decoder.py
+++ ezflow/modules/decoder.py
@@ -6,6 +6,144 @@ from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
 from torch.jit import Final
 from timm.layers import use_fused_attn
 
+import math 
+from torch.nn.init import trunc_normal_ 
+from timm.layers import DropPath
+
+
+class DWConv(nn.Module):
+    def __init__(self, in_features, out_features=None, act_layer=nn.GELU, kernel_size=3):
+        super().__init__()
+        out_features = out_features or in_features
+
+        padding = kernel_size // 2
+
+        self.conv1 = torch.nn.Conv2d(
+            in_features, in_features, kernel_size=kernel_size, padding=padding, groups=in_features)
+        self.act = act_layer()
+        self.bn = nn.BatchNorm2d(in_features)
+        self.conv2 = torch.nn.Conv2d(
+            in_features, out_features, kernel_size=kernel_size, padding=padding, groups=out_features)
+
+    def forward(self, x, H: int, W: int):
+        B, N, C = x.shape
+        x = x.permute(0, 2, 1).reshape(B, C, H, W)
+        x = self.conv1(x)
+        x = self.act(x)
+        x = self.bn(x)
+
+
+        x = self.conv2(x)
+        x = x.reshape(B, C, N).permute(0, 2, 1)
+        return x
+
+
+class WierdMlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., linear=False):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.gamma = nn.Parameter(torch.ones(hidden_features), requires_grad=True)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+        self.linear = linear
+        if self.linear:
+            self.relu = nn.ReLU(inplace=True)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x, H, W):
+        x = self.fc1(x)
+        if self.linear:
+            x = self.relu(x)
+        x = self.drop(self.gamma * self.dwconv(x, H, W)) + x
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class ChannelProcessing(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., linear=False, drop_path=0., 
+                 mlp_hidden_dim=None, act_layer=nn.GELU, drop=None, norm_layer=nn.LayerNorm, cha_sr_ratio=1, c_head_num=None):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+
+        self.dim = dim
+        num_heads = c_head_num or num_heads
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.cha_sr_ratio = cha_sr_ratio if num_heads > 1 else 1
+
+        # config of mlp for v processing
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.mlp_v = Mlp(in_features=dim//self.cha_sr_ratio, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.norm_v = norm_layer(dim//self.cha_sr_ratio)
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.attn_drop = nn.Dropout(attn_drop)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+    def _gen_attn(self, q, k):
+        q = q.softmax(-2).transpose(-1,-2)
+        _, _, N, _  = k.shape
+        k = torch.nn.functional.adaptive_avg_pool2d(k.softmax(-2), (N, 1))
+        
+        attn = torch.nn.functional.sigmoid(q @ k)
+        return attn  * self.temperature
+    def forward(self, x, H=32, W=32, atten=None):
+        B, N, C = x.shape
+        v = x.reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        k = x.reshape(B, N, self.num_heads,  C // self.num_heads).permute(0, 2, 1, 3)
+
+        attn = self._gen_attn(q, k)
+        attn = self.attn_drop(attn)
+
+        Bv, Hd, Nv, Cv = v.shape
+        v = self.norm_v(self.mlp_v(v.transpose(1, 2).reshape(Bv, Nv, Hd*Cv))).reshape(Bv, Nv, Hd, Cv).transpose(1, 2)
+
+        repeat_time = N // attn.shape[-1]
+        attn = attn.repeat_interleave(repeat_time, dim=-1) if attn.shape[-1] > 1 else attn
+        x = (attn * v.transpose(-1, -2)).permute(0, 3, 1, 2).reshape(B, N, C)
+        return x,  (attn * v.transpose(-1, -2)).transpose(-1, -2) #attn
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'temperature'}
 
 
 
@@ -79,7 +217,7 @@ class DecoderBlock(nn.Module):
         
         mlp_hidden_dim = int(hidden_size * mlp_ratio)
         approx_gelu = lambda: nn.GELU(approximate="tanh")
-        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0.3, bias=True)
+        self.mlp = ChannelProcessing(dim=hidden_size, num_heads=num_heads, mlp_hidden_dim=mlp_hidden_dim, act_layer=approx_gelu, drop=0.3)
         self.norm1 = torch.nn.LayerNorm(hidden_size)
         self.norm2 = torch.nn.LayerNorm(hidden_size)
         self.norm3 = torch.nn.LayerNorm(hidden_size)
@@ -103,7 +241,7 @@ class DecoderBlock(nn.Module):
         q = self.norm2(q)
         
         # mlp
-        q = q + self.mlp(q)
+        q = q + self.mlp(q)[0]
         q = self.norm3(q)
         # add gelu here?
         return q
@@ -116,7 +254,7 @@ class ConcatenateDecoderBlock(nn.Module):
         
         mlp_hidden_dim = int(hidden_size * mlp_ratio)
         approx_gelu = lambda: nn.GELU(approximate="tanh")
-        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0.3, bias=True)
+        self.mlp = ChannelProcessing(dim=hidden_size, num_heads=num_heads, mlp_hidden_dim=mlp_hidden_dim, act_layer=approx_gelu, drop=0.3)
         self.norm1 = torch.nn.LayerNorm(hidden_size)
         self.norm2 = torch.nn.LayerNorm(hidden_size)
         self.norm3 = torch.nn.LayerNorm(hidden_size)
@@ -136,7 +274,7 @@ class ConcatenateDecoderBlock(nn.Module):
         q = self.norm2(q)
         
         # mlp
-        q = q + self.mlp(q)
+        q = q + self.mlp(q)[0]
         q = self.norm3(q)
         # add gelu here?
         return q
